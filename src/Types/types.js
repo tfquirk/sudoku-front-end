dc8458c30d4006d03cb51319463d2f63b1136e5e
tfquirk <timothyfquirk@gmail.com>
2019-05-04 13:35:00 -0400

    added types as consts

---
+++ src/Types/types.js
@@ -0,0 +1,6 @@
+// types for store
+
+export const UPDATE_GAMES = "UPDATE_GAMES";
+export const UPDATE_BOARDS = "UPDATE_BOARDS";
+export const CURRENT_PLAYER = "CURRENT_PLAYER";
+export const PLAYER_TOKEN = "PLAYER_TOKEN";

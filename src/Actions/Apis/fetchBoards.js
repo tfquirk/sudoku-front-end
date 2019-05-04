80db1dd095cccbc6734799f655aa9cbd9f064f98
tfquirk <timothyfquirk@gmail.com>
2019-05-04 14:16:41 -0400

    path correction

--- src/Actions/Apis/fetchBoards.js
+++ src/Actions/Apis/fetchBoards.js
@@ -1,5 +1,5 @@
 // import type to be used with dispatches
-import { UPDATE_BOARDS } from "../Types/types";
+import { UPDATE_BOARDS } from "../../Types/types";
 
 // set API endpoints to hit as constants
 const BOARDS_API = "http://localhost:8000/api/v1/boards";

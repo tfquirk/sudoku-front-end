2db29e74927177c3bb6d1a303b259e98fdd57615
tfquirk <timothyfquirk@gmail.com>
2019-05-04 14:17:27 -0400

    created placeholder homepage that will only display a board for now

---
+++ src/Pages/Homepage.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+
+// components found in components folder
+import Board from "../Components/Board";
+
+const Homepage = props => {
+  return (
+    <Fragment>
+      <Board />
+    </Fragment>
+  );
+};
+
+export default Homepage;

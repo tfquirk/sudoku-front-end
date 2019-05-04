5a9069dc83b3367185932de8f53fb9486e58dee5
tfquirk <timothyfquirk@gmail.com>
2019-05-04 13:34:03 -0400

    created fetchBoard action to test api

---
+++ src/Actions/Apis/fetchBoards.js
@@ -0,0 +1,18 @@
+// import type to be used with dispatches
+import { UPDATE_BOARDS } from "../Types/types";
+
+// set API endpoints to hit as constants
+const BOARDS_API = "http://localhost:8000/api/v1/boards";
+
+// fecth to star wars api and get people
+export function fetchBoards(dispatch, endpoint) {
+  return fetch(BOARDS_API)
+    .then(res => res.json())
+    .then(boards => {
+      // update array of 10 people in Redux state
+      dispatch({
+        type: UPDATE_BOARDS,
+        payload: boards
+      });
+    });
+}

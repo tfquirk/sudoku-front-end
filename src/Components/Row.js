d59cf16f6dadcd9fe3ad6bb309f8ea2667da8dc5
tfquirk <timothyfquirk@gmail.com>
2019-05-10 20:48:49 -0400

    solution passed from Row to Column

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -13,25 +13,81 @@ const Row = props => {
   const column7_num = props.numbers.substr(6, 1);
   const column8_num = props.numbers.substr(7, 1);
   const column9_num = props.numbers.substr(8, 1);
+
+  const column1_solution = props.solutions.substr(0, 1);
+  const column2_solution = props.solutions.substr(1, 1);
+  const column3_solution = props.solutions.substr(2, 1);
+  const column4_solution = props.solutions.substr(3, 1);
+  const column5_solution = props.solutions.substr(4, 1);
+  const column6_solution = props.solutions.substr(5, 1);
+  const column7_solution = props.solutions.substr(6, 1);
+  const column8_solution = props.solutions.substr(7, 1);
+  const column9_solution = props.solutions.substr(8, 1);
+
   return (
     <tr>
-      <Column row={props.row} column="column1" number={column1_num} />
+      <Column
+        row={props.row}
+        column="column1"
+        number={column1_num}
+        solution={column1_solution}
+      />
 
-      <Column row={props.row} column="column2" number={column2_num} />
+      <Column
+        row={props.row}
+        column="column2"
+        number={column2_num}
+        solution={column2_solution}
+      />
 
-      <Column row={props.row} column="column3" number={column3_num} />
+      <Column
+        row={props.row}
+        column="column3"
+        number={column3_num}
+        solution={column3_solution}
+      />
 
-      <Column row={props.row} column="column4" number={column4_num} />
+      <Column
+        row={props.row}
+        column="column4"
+        number={column4_num}
+        solution={column4_solution}
+      />
 
-      <Column row={props.row} column="column5" number={column5_num} />
+      <Column
+        row={props.row}
+        column="column5"
+        number={column5_num}
+        solution={column5_solution}
+      />
 
-      <Column row={props.row} column="column6" number={column6_num} />
+      <Column
+        row={props.row}
+        column="column6"
+        number={column6_num}
+        solution={column6_solution}
+      />
 
-      <Column row={props.row} column="column7" number={column7_num} />
+      <Column
+        row={props.row}
+        column="column7"
+        number={column7_num}
+        solution={column7_solution}
+      />
 
-      <Column row={props.row} column="column8" number={column8_num} />
+      <Column
+        row={props.row}
+        column="column8"
+        number={column8_num}
+        solution={column8_solution}
+      />
 
-      <Column row={props.row} column="column9" number={column9_num} />
+      <Column
+        row={props.row}
+        column="column9"
+        number={column9_num}
+        solution={column9_solution}
+      />
     </tr>
   );
 };

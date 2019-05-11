19f4a3b6620191832feeff2d744c2677497dd548
tfquirk <timothyfquirk@gmail.com>
2019-05-10 21:23:20 -0400

    solution now checked against user input. If correct, backgroud-color highlights green

--- src/Components/Column.js
+++ src/Components/Column.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 // // components found in components folder
 // import Board from "../components/Board";
 
 const Column = props => {
+  const correct_answer = props.solution;
+  const [userAnswer, setUserAnswer] = useState("");
+
+  const updateInput = event => {
+    setUserAnswer(event.target.value);
+  };
+
   if (props.number === "0") {
+    if (userAnswer === correct_answer) {
+      return (
+        <td className={`${props.row} ${props.column} green`}>
+          <input onChange={updateInput} />
+        </td>
+      );
+    }
+
     return (
       <td className={`${props.row} ${props.column}`}>
-        <input style={{ height: "2vh", width: "2vh" }} />
+        <input onChange={updateInput} />
       </td>
     );
   } else {
-    return <td className={`${props.row} ${props.column}`}>{props.number}</td>;
+    return (
+      <td className={`${props.row} ${props.column} green`}>{props.number}</td>
+    );
   }
 };
 

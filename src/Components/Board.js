de13bc795469f87748c3a8cfb6a1048d2c8c6973
tfquirk <timothyfquirk@gmail.com>
2019-05-13 14:01:40 -0400

    Refactored Board.js to use decontruction and move logic to props so that the entire component is much cleaner, with less code to dig through

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -17,46 +17,63 @@ const Board = props => {
     // get randomPuzzleId to randomly display a puzzle from all of those
     // returned from the API
     const randomPuzzleId = Math.floor(Math.random() * props.boards.length);
-    const puzzle = props.boards[randomPuzzleId].puzzle;
 
-    // Split the puzzle into its 9 rows
-    const row1 = puzzle.substr(0, 9);
-    const row2 = puzzle.substr(9, 9);
-    const row3 = puzzle.substr(18, 9);
-    const row4 = puzzle.substr(27, 9);
-    const row5 = puzzle.substr(36, 9);
-    const row6 = puzzle.substr(45, 9);
-    const row7 = puzzle.substr(54, 9);
-    const row8 = puzzle.substr(63, 9);
-    const row9 = puzzle.substr(72, 9);
-
-    // get the solution for the random puzzle and split that
-    // information into its 9 rows
-    const solution = props.boards[randomPuzzleId].solution;
-    const row1_solution = solution.substr(0, 9);
-    const row2_solution = solution.substr(9, 9);
-    const row3_solution = solution.substr(18, 9);
-    const row4_solution = solution.substr(27, 9);
-    const row5_solution = solution.substr(36, 9);
-    const row6_solution = solution.substr(45, 9);
-    const row7_solution = solution.substr(54, 9);
-    const row8_solution = solution.substr(63, 9);
-    const row9_solution = solution.substr(72, 9);
+    // deconstruct puzzle and solution based on the randomly assigned board
+    const [puzzle, solution] = [
+      props.boards[randomPuzzleId].puzzle,
+      props.boards[randomPuzzleId].solution
+    ];
 
     return (
       <div className="sudoku-container">
         <div className="sudoku">
           <table>
             <tbody>
-              <Row row="row1" numbers={row1} solutions={row1_solution} />
-              <Row row="row2" numbers={row2} solutions={row2_solution} />
-              <Row row="row3" numbers={row3} solutions={row3_solution} />
-              <Row row="row4" numbers={row4} solutions={row4_solution} />
-              <Row row="row5" numbers={row5} solutions={row5_solution} />
-              <Row row="row6" numbers={row6} solutions={row6_solution} />
-              <Row row="row7" numbers={row7} solutions={row7_solution} />
-              <Row row="row8" numbers={row8} solutions={row8_solution} />
-              <Row row="row9" numbers={row9} solutions={row9_solution} />
+              <Row
+                row="row1"
+                numbers={puzzle.substr(0, 9)}
+                solutions={solution.substr(0, 9)}
+              />
+              <Row
+                row="row2"
+                numbers={puzzle.substr(9, 9)}
+                solutions={solution.substr(9, 9)}
+              />
+              <Row
+                row="row3"
+                numbers={puzzle.substr(18, 9)}
+                solutions={solution.substr(18, 9)}
+              />
+              <Row
+                row="row4"
+                numbers={puzzle.substr(27, 9)}
+                solutions={solution.substr(27, 9)}
+              />
+              <Row
+                row="row5"
+                numbers={puzzle.substr(36, 9)}
+                solutions={solution.substr(36, 9)}
+              />
+              <Row
+                row="row6"
+                numbers={puzzle.substr(45, 9)}
+                solutions={solution.substr(45, 9)}
+              />
+              <Row
+                row="row7"
+                numbers={puzzle.substr(54, 9)}
+                solutions={solution.substr(54, 9)}
+              />
+              <Row
+                row="row8"
+                numbers={puzzle.substr(63, 9)}
+                solutions={solution.substr(63, 9)}
+              />
+              <Row
+                row="row9"
+                numbers={puzzle.substr(72, 9)}
+                solutions={solution.substr(72, 9)}
+              />
             </tbody>
           </table>
         </div>

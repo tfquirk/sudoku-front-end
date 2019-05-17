9faede0e0ce8c561cd2b6bb7238d6495c901890c
tfquirk <timothyfquirk@gmail.com>
2019-05-17 12:56:40 -0400

    Reorganized board to used new styled Table component

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -7,6 +7,7 @@ import { connect } from "react-redux";
 import "./Board.css";
 
 // components found in components folder
+import Table from "./Table";
 import Row from "./Row";
 
 const Board = props => {
@@ -19,14 +20,6 @@ const Board = props => {
   const sudoku = {
     display: "table-cell",
     verticalAlign: "middle"
-  };
-
-  const table = {
-    border: "8px solid #000000",
-    width: "45vw",
-    height: "45vw",
-    margin: "auto",
-    background: "#000000"
   };
 
   if (props.boards.length === 0) {
@@ -46,55 +39,53 @@ const Board = props => {
     return (
       <div style={sudokuContainer}>
         <div style={sudoku}>
-          <table style={table}>
-            <tbody>
-              <Row
-                row="row1"
-                numbers={puzzle.substr(0, 9)}
-                solutions={solution.substr(0, 9)}
-              />
-              <Row
-                row="row2"
-                numbers={puzzle.substr(9, 9)}
-                solutions={solution.substr(9, 9)}
-              />
-              <Row
-                row="row3"
-                numbers={puzzle.substr(18, 9)}
-                solutions={solution.substr(18, 9)}
-              />
-              <Row
-                row="row4"
-                numbers={puzzle.substr(27, 9)}
-                solutions={solution.substr(27, 9)}
-              />
-              <Row
-                row="row5"
-                numbers={puzzle.substr(36, 9)}
-                solutions={solution.substr(36, 9)}
-              />
-              <Row
-                row="row6"
-                numbers={puzzle.substr(45, 9)}
-                solutions={solution.substr(45, 9)}
-              />
-              <Row
-                row="row7"
-                numbers={puzzle.substr(54, 9)}
-                solutions={solution.substr(54, 9)}
-              />
-              <Row
-                row="row8"
-                numbers={puzzle.substr(63, 9)}
-                solutions={solution.substr(63, 9)}
-              />
-              <Row
-                row="row9"
-                numbers={puzzle.substr(72, 9)}
-                solutions={solution.substr(72, 9)}
-              />
-            </tbody>
-          </table>
+          <Table>
+            <Row
+              row="row1"
+              numbers={puzzle.substr(0, 9)}
+              solutions={solution.substr(0, 9)}
+            />
+            <Row
+              row="row2"
+              numbers={puzzle.substr(9, 9)}
+              solutions={solution.substr(9, 9)}
+            />
+            <Row
+              row="row3"
+              numbers={puzzle.substr(18, 9)}
+              solutions={solution.substr(18, 9)}
+            />
+            <Row
+              row="row4"
+              numbers={puzzle.substr(27, 9)}
+              solutions={solution.substr(27, 9)}
+            />
+            <Row
+              row="row5"
+              numbers={puzzle.substr(36, 9)}
+              solutions={solution.substr(36, 9)}
+            />
+            <Row
+              row="row6"
+              numbers={puzzle.substr(45, 9)}
+              solutions={solution.substr(45, 9)}
+            />
+            <Row
+              row="row7"
+              numbers={puzzle.substr(54, 9)}
+              solutions={solution.substr(54, 9)}
+            />
+            <Row
+              row="row8"
+              numbers={puzzle.substr(63, 9)}
+              solutions={solution.substr(63, 9)}
+            />
+            <Row
+              row="row9"
+              numbers={puzzle.substr(72, 9)}
+              solutions={solution.substr(72, 9)}
+            />
+          </Table>
         </div>
       </div>
     );

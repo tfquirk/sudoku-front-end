970918538768a41f00acd7d87ad6a2ca89266d08
tfquirk <timothyfquirk@gmail.com>
2019-05-17 13:10:23 -0400

    created a loading component to show while fetch is happening

---
+++ src/Components/Loading.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Loading = props => {
+  const loadingGif = {
+    width: "45vw",
+    height: "45vw",
+    margin: "auto 27.5vw"
+  };
+
+  return (
+    <img
+      src="https://media.giphy.com/media/l41Yy6jvn3BXYDRu0/giphy.gif"
+      style={loadingGif}
+    />
+  );
+};
+
+export default Loading;

9f6b9dbc083d74d6f614f41e66947a8c6398e30d
tfquirk <timothyfquirk@gmail.com>
2019-05-17 13:35:19 -0400

    Reformated to be a styled component

--- src/Components/Column.js
+++ src/Components/Column.js
@@ -1,9 +1,21 @@
 import React, { useState } from "react";
 
 const Column = props => {
-  const tdStyles = {
+  const tdStyle = {
     padding: "0.5vw",
     textAlign: "center"
+  };
+
+  const inputStyle = {
+    color: "#000000",
+    padding: "0.5vh",
+    border: "0",
+    textAlign: "center",
+    width: "48px",
+    height: "48px",
+    fontSize: "2em",
+    backgroundColor: "#ffffff",
+    outline: "none"
   };
 
   const correct_answer = props.solution;
@@ -16,20 +28,22 @@ const Column = props => {
   if (props.number === "0") {
     if (userAnswer === correct_answer) {
       return (
-        <td className={`${props.row} ${props.column} green`}>
-          <input onChange={updateInput} />
+        <td style={tdStyle} className={`${props.row} ${props.column} green`}>
+          <input style={inputStyle} onChange={updateInput} />
         </td>
       );
     }
 
     return (
-      <td className={`${props.row} ${props.column}`}>
-        <input onChange={updateInput} />
+      <td style={tdStyle} className={`${props.row} ${props.column}`}>
+        <input style={inputStyle} onChange={updateInput} />
       </td>
     );
   } else {
     return (
-      <td className={`${props.row} ${props.column} green`}>{props.number}</td>
+      <td style={tdStyle} className={`${props.row} ${props.column} green`}>
+        {props.number}
+      </td>
     );
   }
 };

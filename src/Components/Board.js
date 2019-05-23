54b56dde5593920f7752f4ff8f1e5a36862a3f17
tfquirk <timothyfquirk@gmail.com>
2019-05-23 13:39:59 -0400

    Board refactored to recieve boards and random board as props, no longer needed to be connected to Redux

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,8 +1,5 @@
 import React from "react";
 
-// connect to Redux state
-import { connect } from "react-redux";
-
 // import CSS
 import "./Board.css";
 
@@ -14,12 +11,6 @@ import Table from "./Table";
 
 const Board = props => {
   // container styles
-  const gameContainer = {
-    minWidth: "98vw",
-    minHeight: "100vh",
-    display: "flex"
-  };
-
   const sudokuContainer = {
     minWidth: "80vw",
     minHeight: "100vh",
@@ -44,79 +35,66 @@ const Board = props => {
 
     // if Redux state holds games, then return a random game
   } else {
-    // get randomPuzzleId to randomly display a puzzle from all of those
-    // returned from the API
-    const randomPuzzleId = Math.floor(Math.random() * props.boards.length);
-
     // deconstruct puzzle and solution based on the randomly assigned board
     const [puzzle, solution] = [
-      props.boards[randomPuzzleId].puzzle,
-      props.boards[randomPuzzleId].solution
+      props.boards[props.randomPuzzleId].puzzle,
+      props.boards[props.randomPuzzleId].solution
     ];
 
     return (
-      <div style={gameContainer}>
-        <Sidebar />
-        <div style={sudokuContainer}>
-          <div style={sudoku}>
-            <Table>
-              <Row
-                row="row1"
-                numbers={puzzle.substr(0, 9)}
-                solutions={solution.substr(0, 9)}
-              />
-              <Row
-                row="row2"
-                numbers={puzzle.substr(9, 9)}
-                solutions={solution.substr(9, 9)}
-              />
-              <Row
-                row="row3"
-                numbers={puzzle.substr(18, 9)}
-                solutions={solution.substr(18, 9)}
-              />
-              <Row
-                row="row4"
-                numbers={puzzle.substr(27, 9)}
-                solutions={solution.substr(27, 9)}
-              />
-              <Row
-                row="row5"
-                numbers={puzzle.substr(36, 9)}
-                solutions={solution.substr(36, 9)}
-              />
-              <Row
-                row="row6"
-                numbers={puzzle.substr(45, 9)}
-                solutions={solution.substr(45, 9)}
-              />
-              <Row
-                row="row7"
-                numbers={puzzle.substr(54, 9)}
-                solutions={solution.substr(54, 9)}
-              />
-              <Row
-                row="row8"
-                numbers={puzzle.substr(63, 9)}
-                solutions={solution.substr(63, 9)}
-              />
-              <Row
-                row="row9"
-                numbers={puzzle.substr(72, 9)}
-                solutions={solution.substr(72, 9)}
-              />
-            </Table>
-          </div>
+      <div style={sudokuContainer}>
+        <div style={sudoku}>
+          <Table>
+            <Row
+              row="row1"
+              numbers={puzzle.substr(0, 9)}
+              solutions={solution.substr(0, 9)}
+            />
+            <Row
+              row="row2"
+              numbers={puzzle.substr(9, 9)}
+              solutions={solution.substr(9, 9)}
+            />
+            <Row
+              row="row3"
+              numbers={puzzle.substr(18, 9)}
+              solutions={solution.substr(18, 9)}
+            />
+            <Row
+              row="row4"
+              numbers={puzzle.substr(27, 9)}
+              solutions={solution.substr(27, 9)}
+            />
+            <Row
+              row="row5"
+              numbers={puzzle.substr(36, 9)}
+              solutions={solution.substr(36, 9)}
+            />
+            <Row
+              row="row6"
+              numbers={puzzle.substr(45, 9)}
+              solutions={solution.substr(45, 9)}
+            />
+            <Row
+              row="row7"
+              numbers={puzzle.substr(54, 9)}
+              solutions={solution.substr(54, 9)}
+            />
+            <Row
+              row="row8"
+              numbers={puzzle.substr(63, 9)}
+              solutions={solution.substr(63, 9)}
+            />
+            <Row
+              row="row9"
+              numbers={puzzle.substr(72, 9)}
+              solutions={solution.substr(72, 9)}
+            />
+          </Table>
         </div>
       </div>
     );
   }
 };
 
-function mapStateToProps(state) {
-  return {
-    boards: state.boards
-  };
-}
-
-export default connect(mapStateToProps)(Board);
+export default Board;

9ac75cd71c84781d65b141e01827ee7682457b19
tfquirk <timothyfquirk@gmail.com>
2019-05-23 15:38:22 -0400

    connected to redux and conditional render now handled by this container

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,7 +1,11 @@
 import React from "react";
 
+// connect to Redux state
+import { connect } from "react-redux";
+
 // components found in components folder
 import Board from "../Components/Board";
+import Loading from "../Components/Loading";
 import Sidebar from "../Components/Sidebar";
 
 const Homepage = props => {
@@ -11,12 +15,33 @@ const Homepage = props => {
     display: "flex"
   };
 
-  return (
-    <div style={gameContainer}>
-      <Sidebar />
-      <Board />
-    </div>
-  );
+  // get randomPuzzleId to randomly display a puzzle from all of those
+  // returned from the API
+  const randomPuzzleId = Math.floor(Math.random() * props.boards.length);
+
+  // if Redux state does not hold any games, return a loading gif
+  if (props.boards.length === 0) {
+    return (
+      <div style={gameContainer}>
+        <Loading />
+      </div>
+    );
+
+    // if Redux state holds games, then return a random game
+  } else {
+    return (
+      <div style={gameContainer}>
+        <Sidebar />
+        <Board randomPuzzleId={randomPuzzleId} boards={props.boards} />
+      </div>
+    );
+  }
 };
 
-export default Homepage;
+function mapStateToProps(state) {
+  return {
+    boards: state.boards
+  };
+}
+
+export default connect(mapStateToProps)(Homepage);

85930054ab61f0d934a981f59f1db7fe92b04a70
tfquirk <timothyfquirk@gmail.com>
2019-05-23 15:37:20 -0400

    conditional render logic moved to container

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -23,78 +23,65 @@ const Board = props => {
     verticalAlign: "middle"
   };
 
-  // if Redux state does not hold any games, return a loading gif
-  if (props.boards.length === 0) {
-    return (
-      <div style={sudokuContainer}>
-        <div style={sudoku}>
-          <Loading />
-        </div>
-      </div>
-    );
+  // deconstruct puzzle and solution based on the randomly assigned board
+  const [puzzle, solution] = [
+    props.boards[props.randomPuzzleId].puzzle,
+    props.boards[props.randomPuzzleId].solution
+  ];
 
-    // if Redux state holds games, then return a random game
-  } else {
-    // deconstruct puzzle and solution based on the randomly assigned board
-    const [puzzle, solution] = [
-      props.boards[props.randomPuzzleId].puzzle,
-      props.boards[props.randomPuzzleId].solution
-    ];
-
-    return (
-      <div style={sudokuContainer}>
-        <div style={sudoku}>
-          <Table>
-            <Row
-              row="row1"
-              numbers={puzzle.substr(0, 9)}
-              solutions={solution.substr(0, 9)}
-            />
-            <Row
-              row="row2"
-              numbers={puzzle.substr(9, 9)}
-              solutions={solution.substr(9, 9)}
-            />
-            <Row
-              row="row3"
-              numbers={puzzle.substr(18, 9)}
-              solutions={solution.substr(18, 9)}
-            />
-            <Row
-              row="row4"
-              numbers={puzzle.substr(27, 9)}
-              solutions={solution.substr(27, 9)}
-            />
-            <Row
-              row="row5"
-              numbers={puzzle.substr(36, 9)}
-              solutions={solution.substr(36, 9)}
-            />
-            <Row
-              row="row6"
-              numbers={puzzle.substr(45, 9)}
-              solutions={solution.substr(45, 9)}
-            />
-            <Row
-              row="row7"
-              numbers={puzzle.substr(54, 9)}
-              solutions={solution.substr(54, 9)}
-            />
-            <Row
-              row="row8"
-              numbers={puzzle.substr(63, 9)}
-              solutions={solution.substr(63, 9)}
-            />
-            <Row
-              row="row9"
-              numbers={puzzle.substr(72, 9)}
-              solutions={solution.substr(72, 9)}
-            />
-          </Table>
-        </div>
+  return (
+    <div style={sudokuContainer}>
+      <div style={sudoku}>
+        <Table>
+          <Row
+            row="row1"
+            numbers={puzzle.substr(0, 9)}
+            solutions={solution.substr(0, 9)}
+          />
+          <Row
+            row="row2"
+            numbers={puzzle.substr(9, 9)}
+            solutions={solution.substr(9, 9)}
+          />
+          <Row
+            row="row3"
+            numbers={puzzle.substr(18, 9)}
+            solutions={solution.substr(18, 9)}
+          />
+          <Row
+            row="row4"
+            numbers={puzzle.substr(27, 9)}
+            solutions={solution.substr(27, 9)}
+          />
+          <Row
+            row="row5"
+            numbers={puzzle.substr(36, 9)}
+            solutions={solution.substr(36, 9)}
+          />
+          <Row
+            row="row6"
+            numbers={puzzle.substr(45, 9)}
+            solutions={solution.substr(45, 9)}
+          />
+          <Row
+            row="row7"
+            numbers={puzzle.substr(54, 9)}
+            solutions={solution.substr(54, 9)}
+          />
+          <Row
+            row="row8"
+            numbers={puzzle.substr(63, 9)}
+            solutions={solution.substr(63, 9)}
+          />
+          <Row
+            row="row9"
+            numbers={puzzle.substr(72, 9)}
+            solutions={solution.substr(72, 9)}
+          />
+        </Table>
       </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default Board;

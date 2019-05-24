fb8958fd1d154e458d1cada7233cf01d2fb75d44
tfquirk <timothyfquirk@gmail.com>
2019-05-24 17:07:41 -0400

    initial outline for game information

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -5,10 +5,27 @@ const Sidebar = props => {
     margin: 0,
     width: "25vw",
     height: "100vh",
-    backgroundColor: "Red"
+    backgroundColor: "Red",
+    color: "white"
   };
 
-  return <div style={sidebarStyles}>{props.children}</div>;
+  return (
+    <div style={sidebarStyles}>
+      <div
+        style={{
+          border: "1px solid white",
+          margin: "auto",
+          width: "15vw",
+          height: "25vh"
+        }}
+      >
+        <h1>Game #: {`${props.board.id}/5000`}</h1>
+        <p>
+          Quickest time completed: {props.board.quickest_time_completed || "NA"}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;

74ba610c1e571f9c4a53a78d313839981a150bd9
tfquirk <timothyfquirk@gmail.com>
2019-05-28 15:36:01 -0400

    added comments to the Column component logic

--- src/Components/Column.js
+++ src/Components/Column.js
@@ -7,11 +7,13 @@ import { connect } from "react-redux";
 import { COMPLETED_TILE } from "../Types/types";
 
 const Column = props => {
+  // styles for each column in the given row
   const tdStyle = {
     padding: "0.5vw",
     textAlign: "center"
   };
 
+  // specific styling for the input displayed in the column
   const inputStyle = {
     color: "#000000",
     padding: "0.5vh",
@@ -24,6 +26,7 @@ const Column = props => {
     outline: "none"
   };
 
+  // style for when user enters correct input
   const inputStyleGreen = {
     color: "#000000",
     padding: "0.5vh",
@@ -36,23 +39,35 @@ const Column = props => {
     outline: "none"
   };
 
+  // local variable to check the proper solution for this column
   const correct_answer = props.solution;
+  // local variable to hold the users answer
   const [userAnswer, setUserAnswer] = useState("");
+  // boolean value to check the status of this tile, has it been completed?
+  // A correct answer will change the value of this to true
   const [tileCompleted, setTileCompleted] = useState(false);
 
   const updateInput = event => {
+    // updates the value of users answer upon change in the input field
     setUserAnswer(event.target.value);
   };
 
-  console.log(props.completedTiles);
-
+  // if the number prop holds a value of zero, then the user is required
+  // to enter a solution for this tile.
   if (props.number === "0") {
+    // if the user enters a correct value in the column tile return the following
     if (userAnswer === correct_answer) {
+      // the first time the user enters the correct value execute the following
       if (tileCompleted === false) {
+        // increse the redux state value of the correct tiles by 1
         props.completedTileIncrease();
+        // change the boolean value for the local state of this column to true
+        // this will prevent the redux state of completedTiles from being
+        // incresed each time there is a re-render of this component
         setTileCompleted(true);
       }
 
+      // return the column tile with the correct number and styling for a correct answer
       return (
         <td style={tdStyle} className={`${props.row} ${props.column} green`}>
           <input style={inputStyleGreen} onChange={updateInput} />
@@ -60,17 +75,26 @@ const Column = props => {
       );
     }
 
+    // return the tile with the blank or incorrect value for this column tile
     return (
       <td style={tdStyle} className={`${props.row} ${props.column}`}>
         <input style={inputStyle} onChange={updateInput} />
       </td>
     );
   } else {
+    // in the case that this column tile aka props.number !== 0
+
+    // change the boolean value of this column tile to show that it has the correct answer
     if (tileCompleted === false) {
+      // increse the redux state value of the correct tiles by 1
       props.completedTileIncrease();
+      // change the boolean value for the local state of this column to true
+      // this will prevent the redux state of completedTiles from being
+      // incresed each time there is a re-render of this component
       setTileCompleted(true);
     }
 
+    // return the correct_answer in the column tile with the correct styling
     return (
       <td style={tdStyle} className={`${props.row} ${props.column} green`}>
         {props.number}
@@ -79,11 +103,11 @@ const Column = props => {
   }
 };
 
-function mapStateToProps(state) {
-  return {
-    completedTiles: state.completedTiles
-  };
-}
+// function mapStateToProps(state) {
+//   return {
+//     completedTiles: state.completedTiles
+//   };
+// }
 
 function mapDispatchToProps(dispatch) {
   return {
@@ -92,6 +116,6 @@ function mapDispatchToProps(dispatch) {
 }
 
 export default connect(
-  mapStateToProps,
+  null,
   mapDispatchToProps
 )(Column);
